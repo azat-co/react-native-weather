04ff4e1f68d6202ff71aee6c15460a6b094ebe3d
Azat Mardan <hi@azat.co>
2017-10-11 10:46:02 -0700

    add changes :japanese_goblin:

--- App.js
+++ App.js
@@ -6,10 +6,30 @@ import { StyleSheet,
  } from 'react-native';
 
 export default class App extends React.Component {
+  constructor(props) {
+    super(props)
+    this.state = {cityName: ''}
+    this.handleCityChange = this.handleCityChange.bind(this)
+    this.search = this.search.bind(this)
+  }
+  handleCityChange(text) {
+    this.setState({cityName: text})
+  }
+  search() {
+    console.log(this.state.cityName)
+  }
   render() {
     return (
       <View style={styles.container}>
-        <Text>Open up App.js to start working on your app!!!</Text>
+      <Text>Enter your city for the weather forecast</Text>
+        <TextInput
+          style={styles.citySearchStyle}
+          placeholder="San Francisco"
+          onChangeText={this.handleCityChange}
+          onEndEditing={this.search}
+          value={this.state.cityName}
+          returnKeyType="search">
+        </TextInput>
 
         <Text>Changes you make will automatically reload.</Text>
         <Text>Shake your phone to open the developer menu.</Text>
@@ -25,4 +45,13 @@ const styles = StyleSheet.create({
     alignItems: 'center',
     justifyContent: 'center',
   },
+  citySearchStyle: {
+    borderColor: '#8E8E93',
+    borderWidth: 0.5,
+    backgroundColor: '#fff',
+    height: 40,
+    marginLeft:60,
+    marginRight: 60,
+    padding: 8
+  }
 });
