25f624c78dd722b7e263ccd1f82a57b15aaf9b81
Azat Mardan <hi@azat.co>
2017-10-11 11:49:45 -0700

    add changes :waning_gibbous_moon:

--- App.js
+++ App.js
@@ -2,13 +2,14 @@ import React from 'react';
 import { StyleSheet, 
   Text, 
   View,
-  TextInput
+  TextInput,
+  FlatList
  } from 'react-native';
 
 export default class App extends React.Component {
   constructor(props) {
     super(props)
-    this.state = {cityName: ''}
+    this.state = {cityName: '', forecast: null}
     this.handleCityChange = this.handleCityChange.bind(this)
     this.search = this.search.bind(this)
   }
@@ -17,14 +18,21 @@ export default class App extends React.Component {
   }
   search() {
     console.log(this.state.cityName)
-    fetch(`http://api.openweathermap.org/data/2.5/forecast?q=${this.state.cityName}&mode=json&appid=6ea35fbcd51d5e917b7d8309292bc51c`,{
+    fetch(`http://api.openweathermap.org/data/2.5/forecast?q=${this.state.cityName}&mode=json&appid=6ea35fbcd51d5e917b7d8309292bc51c&units=imperial`,{
       method: 'GET'})
       .then(response=>response.json())
       .then(data=>{
         console.log(data)
+        this.setState({forecast: data})
       })  
   }
   render() {
+    let forecastItem = item=>{
+      item = item.item
+      console.log(item)
+   return <Text>
+      {item.dt_txt}-{item.main.temp}F-{item.weather[0].description}</Text>
+    }
     return (
       <View style={styles.container}>
       <Text>Enter your city for the weather forecast</Text>
@@ -37,8 +45,11 @@ export default class App extends React.Component {
           returnKeyType="search">
         </TextInput>
 
-        <Text>Changes you make will automatically reload.</Text>
-        <Text>Shake your phone to open the developer menu.</Text>
+        {(this.state.forecast)?<FlatList
+          data={this.state.forecast.list}
+          renderItem={forecastItem}
+        ></FlatList>:<Text/>}
+        
       </View>
     );
   }
